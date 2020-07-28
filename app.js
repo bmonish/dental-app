e608cc526dbb3c6cba7a45cbf51becf32036f204
bmonish <monishmonib@gmail.com>
2020-07-28 23:38:42 +0530

    File Change

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw0N33ndxuqj7bZLrY5rThMiDBQL8ulU6p9CfMTW22v-OSRsDE/exec'
 const form = document.forms['submit-to-google-sheet']
+const fadeForm = document.getElementById("survey-form")
+
 
   form.addEventListener('submit', e => {
     e.preventDefault()
-    // setTimeout(target.style.opacity = '0', 3000);
+    document.getElementById("submit-div").style.display="none";
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(() => fadeForm.reset())
       .then(response => window.location="thanks.html")
       .catch(error => console.error('Error!', error.message))
+      console.log("hello")
   })
