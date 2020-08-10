1809ae2ee8ccd7ca8448457e615e7f8de7fe0217
bmonish <monishmonib@gmail.com>
2020-08-10 14:50:34 +0530

    Alert added

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw0N33ndxuqj7bZLrY5rThMiDBQL8ulU6p9CfMTW22v-OSRsDE/exec'
-const form = document.forms['submit-to-google-sheet']
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbw0N33ndxuqj7bZLrY5rThMiDBQL8ulU6p9CfMTW22v-OSRsDE/exec";
+const form = document.forms["submit-to-google-sheet"];
 
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    document.getElementById("submit-div").style.display="none";
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => window.location="thanks.html")
-      .catch(error => console.error('Error!', error.message))
-      console.log("hello")
-  })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.getElementById("submit-div").style.display = "none";
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => (window.location = "thanks.html"))
+    .catch((error) => console.error("Error!", error.message));
+  alert("Survey Submitted Successfully!");
+});
